c1df816508acbf510220169512193d5a2b63a6a1
Alex Zapata <alexzapata1984@gmail.com>
2024-12-20 22:03:22 +0100

    Modify image file link paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,20 +31,20 @@ module.exports = {
 		},
     extend: {
       backgroundImage: {
-        'sanFrancisco': "url('../assets/images/sanFrancisco.jpg')",
-        'sanFranciscoDesktop': "url('../images/sanFranciscoDesktop.jpg')",
-        'yosemite': "url('/src/assets/images/yosemite.jpg')",
-        'LA': "url('/src/assets/images/LA.jpg')",
-        'seattle': "url('/src/assets/images/seattle.jpg')",
+        'sanFrancisco': "url('../../src/assets/images/sanFrancisco.jpg')",
+        'sanFranciscoDesktop': "url('../../src/assets/images/sanFranciscoDesktop.jpg')",
+        'yosemite': "url('../../src/assets/images/yosemite.jpg')",
+        'LA': "url('../../src/assets/images/LA.jpg')",
+        'seattle': "url('../../src/assets/images/seattle.jpg')",
         'new_york': "url('../../src/assets/images/new_york.jpg')",
-        'norway': "url('../assets/images/norway.jpg')",
-        'sydney': "url('/src/assets/images/sydney.jpg')",
-        'miami': "url('/src/assets/images/miami.jpg')",
-        'switzerland': "url('/src/assets/images/switzerland.jpg')",
-        'bali': "url('/src/assets/images/bali.jpg')",        
-        'chicago': "url('/src/assets/images/chicago.jpg')",
-        'europe': "url('/src/assets/images/europe.jpg')",
-        'iceland': "url('/src/assets/images/iceland.jpg')"
+        'norway': "url('../../assets/images/norway.jpg')",
+        'sydney': "url('../../src/assets/images/sydney.jpg')",
+        'miami': "url('../../src/assets/images/miami.jpg')",
+        'switzerland': "url('../../src/assets/images/switzerland.jpg')",
+        'bali': "url('../../src/assets/images/bali.jpg')",        
+        'chicago': "url('../../src/assets/images/chicago.jpg')",
+        'europe': "url('../../src/assets/images/europe.jpg')",
+        'iceland': "url('../../src/assets/images/iceland.jpg')"
       }
     },
   },
