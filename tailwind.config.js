23e58ee4f45a13b040f685c60d535f752f214eca
Alex Zapata <alexzapata1984@gmail.com>
2024-12-20 22:01:11 +0100

    Modify image file link paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,12 +32,12 @@ module.exports = {
     extend: {
       backgroundImage: {
         'sanFrancisco': "url('../assets/images/sanFrancisco.jpg')",
-        'sanFranciscoDesktop': "url('/src/assets/images/sanFranciscoDesktop.jpg')",
+        'sanFranciscoDesktop': "url('../images/sanFranciscoDesktop.jpg')",
         'yosemite': "url('/src/assets/images/yosemite.jpg')",
         'LA': "url('/src/assets/images/LA.jpg')",
         'seattle': "url('/src/assets/images/seattle.jpg')",
-        'new_york': "url('/src/assets/images/new_york.jpg')",
-        'norway': "url('/src/assets/images/norway.jpg')",
+        'new_york': "url('../../src/assets/images/new_york.jpg')",
+        'norway': "url('../assets/images/norway.jpg')",
         'sydney': "url('/src/assets/images/sydney.jpg')",
         'miami': "url('/src/assets/images/miami.jpg')",
         'switzerland': "url('/src/assets/images/switzerland.jpg')",
